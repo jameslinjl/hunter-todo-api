3f18ce2155ce194deb39d5e2689266bf27f4c76e
James Lin <james@level.com>
2020-01-20 17:34:50 -0500

    add heroku deploy changes

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.post('/auth', (req, res) => {
+  // check for username in req
+  // check db for username
+
+  const token = 'asillytoken';
+  res.header('set-cookie', `token=${token}`).json({ token });
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(3000);
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
