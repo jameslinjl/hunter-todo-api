a4fcefaf13074e0011b84b224cd331fcdd53f3b7
James Lin <james@level.com>
2020-01-20 22:11:13 -0500

    got user service workin g

--- src/app.js
+++ src/app.js
@@ -1,6 +1,70 @@
 const express = require('express');
 const app = express();
+
+const bodyParser = require('body-parser');
+const knex = require('knex')({
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING,
+});
+const db = require('./db');
+const lodash = require('lodash');
+
 const PORT = process.env.PORT || 3000;
+
+// parse application/json
+app.use(bodyParser.json());
+
+// handle JSON syntax errors
+app.use((error, req, res, next) => {
+  if (error instanceof SyntaxError) {
+    return res.status(400).json({ error: 'body must be valid JSON' });
+  } else {
+    next();
+  }
+});
+
+app.post('/user', async (req, res) => {
+  const { body } = req;
+
+  if (!body.hasOwnProperty('username')) {
+    return res.status(400).json({ error: 'you must supply a username when creating a user' });
+  }
+
+  const { username } = body;
+
+  try {
+    const usernameCheckRows = await knex(db.USER_TABLE_NAME).where({ username });
+    if (usernameCheckRows.length > 0) {
+      return res.status(400).json({ error: `username "${username}" already exists` });
+    }
+
+    await knex(db.USER_TABLE_NAME).insert({ username });
+
+    const responseData = await knex(db.USER_TABLE_NAME).where({ username });
+    if (responseData.length === 1) {
+      return res.status(201).json(responseData[0]);
+    }
+    return res.status(500).json({ error: 'something weird happened with the API. contact james' });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: 'something weird happened with the API. contact james' });
+  }
+});
+
+app.get('/user', (req, res) => {
+  const usernameQuery = req.query['username'];
+  const selectWhereClause = lodash.isNil(usernameQuery) ? {} : { username: usernameQuery };
+
+  knex(db.USER_TABLE_NAME)
+    .where(selectWhereClause)
+    .then((rows) => {
+      res.status(rows.length > 0 ? 200 : 404).json(rows);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(500).json({ error: 'something weird happened with the API. contact james' });
+    });
+});
 
 app.post('/auth', (req, res) => {
   // check for username in req
---
+++ src/db.js
@@ -0,0 +1,37 @@
+const knex = require('knex')({
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING,
+});
+
+const USER_TABLE_NAME = 'todo_user';
+
+const createTables = () => {
+  knex.schema
+    .createTable(USER_TABLE_NAME, (table) => {
+      table.increments('id');
+      table.string('username');
+    })
+    .then(() => {
+      console.log(`success - created ${USER_TABLE_NAME} table`);
+    })
+    .finally(() => {
+      knex.destroy();
+    });
+};
+
+const dropTables = () => {
+  knex.schema
+    .dropTableIfExists(USER_TABLE_NAME)
+    .then(() => {
+      console.log(`success - dropped ${USER_TABLE_NAME} table`);
+    })
+    .finally(() => {
+      knex.destroy();
+    });
+};
+
+module.exports = {
+  createTables,
+  dropTables,
+  USER_TABLE_NAME,
+};
